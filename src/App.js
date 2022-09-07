7998163868b3a1a7e5e574ebc9850930d1493cf0
Thiago Martins <thiagoamartins0@gmail.com>
2022-09-06 21:03:33 -0300

    Add style guide

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import './App.css';
+import "./styles/settings/colors.css";
+import "./styles/settings/fonts.css";
+import "./styles/generic/reset.css";
+import "./styles/elements/base.css";
 import Logo from "./components/logo/logo";
 
 function App() {
