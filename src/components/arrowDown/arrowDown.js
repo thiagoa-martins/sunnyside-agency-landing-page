4ccc78d1a2140f3bd67f6842f8b6a9683e49138e
Thiago Martins <thiagoamartins0@gmail.com>
2022-09-10 23:21:33 -0300

    Add arrowDown component with CSS

---
+++ src/components/arrowDown/arrowDown.js
@@ -0,0 +1,9 @@
+import "./arrowDown.css";
+import IconArrow from "../../images/icon-arrow-down.svg";
+
+const ArrowDown = () => {
+    return <img className="arrow-down" src={IconArrow}
+    alt="Ícone arrow down" title="Ícone arrow down" />;
+};
+
+export default ArrowDown;
