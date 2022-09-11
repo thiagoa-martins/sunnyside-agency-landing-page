3852bc96878c655db41b2f2fba74e41ce603b6c1
Thiago Martins <thiagoamartins0@gmail.com>
2022-09-11 16:30:30 -0300

    Add cardServices component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import "./styles/settings/fonts.css";
 import "./styles/generic/reset.css";
 import "./styles/elements/base.css";
 import Header from "./objects/header/header";
+import CardServices from "./components/cardServices/cardServices";
 
 function App() {
   return (
     <>
       <Header />
+      <CardServices>
+        <h2>Transform your brand</h2>
+
+        <p>
+          We are a full-service creative agency specializing in helping brands
+          grow fast. Engage your clients through compelling visuals that do most
+          of the marketing for you.
+        </p>
+      </CardServices>
     </>
   );
 }
---
+++ src/components/cardServices/cardServices.js
@@ -0,0 +1,10 @@
+const CardServices = ({ children }) => {
+    return (
+        <div className="card-services">
+            {children}            
+            <span>learn more</span>
+        </div>
+    );
+};
+
+export default CardServices;
