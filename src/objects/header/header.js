02fd757bd9a54e2d865cbd133657cc544c8b0b2b
Thiago Martins <thiagoamartins0@gmail.com>
2022-09-10 23:22:51 -0300

    Add arrowDown component in the header object

--- src/objects/header/header.js
+++ src/objects/header/header.js
@@ -1,12 +1,14 @@
 import "./header.css";
 import Head from "../../objects/head/head";
 import TitleMain from "../../components/titleMain/titleMain";
+import ArrowDown from "../../components/arrowDown/arrowDown";
 
 const Header = () => {
     return (
         <header className="header">
             <Head />
             <TitleMain />
+            <ArrowDown />
         </header>
     );
 };
