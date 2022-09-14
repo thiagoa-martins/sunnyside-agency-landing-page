ffc98d37dac5f3bf822e8f4e1a5b21a5f3a45f8b
Thiago Martins <thiagoamartins0@gmail.com>
2022-09-14 11:02:47 -0300

    Update color prop in the Logo component

--- src/objects/footer/footer.js
+++ src/objects/footer/footer.js
@@ -1,10 +1,19 @@
 import "./footer.css";
 import Gallery from "../gallery/gallery";
+import Logo from "../../components/logo/logo";
+import Menu from "../../components/menu/menu";
+import Socials from "../../components/socials/socials";
 
 const Footer = () => {
     return (
         <footer className="footer">
             <Gallery />
+
+            <section>
+                <Logo color="var(--color-primary-dark-desaturated-cyan)" />
+                <Menu />
+                <Socials />
+            </section>
         </footer>
     );
 };
