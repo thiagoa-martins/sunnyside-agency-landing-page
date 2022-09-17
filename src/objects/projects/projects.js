850ee4e25f46ff89b2eecb2b369e50b89859b232
Thiago Martins <thiagoamartins0@gmail.com>
2022-09-17 13:08:57 -0300

    Add mobile images in the projects object

--- src/objects/projects/projects.js
+++ src/objects/projects/projects.js
@@ -4,42 +4,40 @@ import ImageGraphicDesign from "../../images/desktop/image-graphic-design.jpg";
 import ImagePhotography from "../../images/desktop/image-photography.jpg";
 
 const Projects = () => {
-    return (
-        <section className="projects" id="projects">
-            <div className="project graphic-design">
-                {/* <img src={ImageGraphicDesign}
-                alt="Graphic design"
-                title="Graphic design" />
-                 */}
-                <CardProject
-                color="hsl(167, 40%, 24%)">
-                    <h3>Graphic design</h3>
+  return (
+    <section className="projects" id="projects">
+      <div className="project graphic-design">
+        <img
+          src={ImageGraphicDesign}
+          alt="Graphic design"
+          title="Graphic design"
+        />
 
-                    <p>
-                        Great design makes you memorable. We deliver artwork that
-                        underscores your brand message and captures potential clients’
-                        attention.
-                    </p>
-                </CardProject>
-            </div>
+        <CardProject color="hsl(167, 40%, 24%)">
+          <h3>Graphic design</h3>
 
-            <div className="project">
-                <img src={ImagePhotography}
-                alt="Photography"
-                title="Photography" />
+          <p>
+            Great design makes you memorable. We deliver artwork that
+            underscores your brand message and captures potential clients’
+            attention.
+          </p>
+        </CardProject>
+      </div>
 
-                <CardProject
-                color="hsl(198, 62%, 26%)">
-                    <h3>Photography</h3>
+      <div className="project photography">
+        <img src={ImagePhotography} alt="Photography" title="Photography" />
 
-                    <p>
-                        Increase your credibility by getting the most stunning,
-                        high-quality photos that improve your business image.
-                    </p>
-                </CardProject>
-            </div>
-        </section>
-    );
+        <CardProject color="hsl(198, 62%, 26%)">
+          <h3>Photography</h3>
+
+          <p>
+            Increase your credibility by getting the most stunning, high-quality
+            photos that improve your business image.
+          </p>
+        </CardProject>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
