5b5dcc184dbdf6b790fe4b492ef375ac5650d228
Thiago Martins <thiagoamartins0@gmail.com>
2022-09-17 16:22:52 -0300

    Add padding in the mobile testimonials

--- src/objects/testimonials/testimonials.js
+++ src/objects/testimonials/testimonials.js
@@ -6,50 +6,43 @@ import ImageThomas from "../../images/image-thomas.jpg";
 import ImageJennie from "../../images/image-jennie.jpg";
 
 const Testimonials = () => {
-    return (
-        <section className="testimonials">
-            <TitleTestimonial label="Client testimonials" />
+  return (
+    <section className="testimonials">
+      <TitleTestimonial label="Client testimonials" />
 
-            <div className="cards">
-                <ClientTestimonial src={ImageEmily}
-                alt="Emily"
-                title="Emily">
-                    <p>
-                        We put our trust in Sunnyside and they delivered, making
-                        sure our needs were met and deadlines were always hit.
-                    </p>
+      <div className="cards">
+        <ClientTestimonial src={ImageEmily} alt="Emily" title="Emily">
+          <p>
+            We put our trust in Sunnyside and they delivered, making sure our
+            needs were met and deadlines were always hit.
+          </p>
 
-                    <h4>Emily R.</h4>
-                    <span>Marketing Director</span>
-                </ClientTestimonial>
+          <h4>Emily R.</h4>
+          <span>Marketing Director</span>
+        </ClientTestimonial>
 
-                <ClientTestimonial src={ImageThomas}
-                alt="Thomas"
-                title="Thomas">
-                    <p>
-                        Sunnyside’s enthusiasm coupled with their keen interest in
-                        our brand’s success made it a satisfying and enjoyable
-                        experience.
-                    </p>
+        <ClientTestimonial src={ImageThomas} alt="Thomas" title="Thomas">
+          <p>
+            Sunnyside’s enthusiasm coupled with their keen interest in our
+            brand’s success made it a satisfying and enjoyable experience.
+          </p>
 
-                    <h4>Thomas S.</h4>
-                    <span>Chief Operating Officer</span>
-                </ClientTestimonial>
+          <h4>Thomas S.</h4>
+          <span>Chief Operating Officer</span>
+        </ClientTestimonial>
 
-                <ClientTestimonial src={ImageJennie}
-                alt="Jennie"
-                title="Jennie">
-                    <p>
-                        Incredible end result! Our sales increased over 400% when we
-                        worked with Sunnyside. Highly recommended!
-                    </p>
+        <ClientTestimonial src={ImageJennie} alt="Jennie" title="Jennie">
+          <p>
+            Incredible end result! Our sales increased over 400% when we worked
+            with Sunnyside. Highly recommended!
+          </p>
 
-                    <h4>Jennie F.</h4>
-                    <span>Business Owner</span>
-                </ClientTestimonial>
-            </div>
-        </section>
-    );
+          <h4>Jennie F.</h4>
+          <span>Business Owner</span>
+        </ClientTestimonial>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
